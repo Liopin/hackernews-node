9b533787ecbd2aaa36f093c58b8239e6ef95f396
Liopin <contact.pinheiro.lionel@gmail.com>
2020-05-01 16:03:44 +0200

    Mutation

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url:String!
-}
-`
 
 //Data
 
 let links = [{
     id: 'link-O',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
+    description: 'Fullstack tutorial for GraphQL',
+    url: 'www.howtographql.com'
 }]
 
+let idCount = links.length
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hachernews Clone`,
         feed: () => links,
     },
+    Mutation: {
+        post: (parent, args) => {
+        const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+        }
+            links.push(link)
+            return link
+        }
+    },
 
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 server.start(() => console.log(`Server is running on port 4000`))
