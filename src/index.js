07491785c01d5bcd4c644c6a6bb2fb4b9cb9e302
Liopin <contact.pinheiro.lionel@gmail.com>
2020-05-01 15:17:19 +0200

    Simple query

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url:String!
 }
 `
+
+//Data
+
+let links = [{
+    id: 'link-O',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
+
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hachernews Clone`
+        info: () => `This is the API of a Hachernews Clone`,
+        feed: () => links,
+    },
+
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
